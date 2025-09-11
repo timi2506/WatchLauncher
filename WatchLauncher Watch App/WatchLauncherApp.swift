83fc9033e30e6a90b9274418f4eed48b90e9c620
timi2506 <172171055+timi2506@users.noreply.github.com>
2025-09-11 19:14:38 +0200

    Initial Commit

---
+++ WatchLauncher Watch App/WatchLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLauncherApp.swift
+//  WatchLauncher Watch App
+//
+//  Created by Tim Schuchardt on 11.09.25.
+//
+
+import SwiftUI
+
+@main
+struct WatchLauncher_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
